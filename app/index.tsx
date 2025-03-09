1e233f9b7bda637d3029b1922dae63ba03864695
rafavini <rafaelgerminari@hotmail.com>
2025-03-09 07:58:18 -0300

    Adicionando theme ao tailwind e algumas cores

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-blue-500">teste</Text>
+      <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#030014',
+        secondary: '#151312',
+        light: {
+          100: '#D6C6FF',
+          200: '#A8B5DB',
+          300: '#9CA4AB',
+        },
+        dark: {
+          100: '#221f3d',
+          200: '#0f0d23',
+        },
+        accent: '#AB8BFF'
+
+      }
+    },
   },
   plugins: [],
 }
