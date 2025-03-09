1721a94e7183d2ad2e666bd82eab4c7bdc0167dd
rafavini <rafaelgerminari@hotmail.com>
2025-03-09 07:47:11 -0300

    Primeiro commit - adicionando a base

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-blue-500">teste</Text>
+    </View>
+  );
+}
