d59c6efef0589b4bbe1dc186f7c819771ae52918
Josué <josuealexandre067@gmail.com>
2025-03-10 15:31:49 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 
 export default function Index() {
   return (
+
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
+      <Image className="rounded-full w-80 h-80" source={{uri: 'https://github.com/rafavini.png'}} />
+
+      <Text className="text-5xl text-dark-200 font-bold">Rafael Vinícius</Text>
+
+      <Text className="text-2xl text-dark-200">idade: 18 anos</Text>
+      <Text className="text-2xl text-dark-200">Endereço: Rua 26 de Agosto, 300</Text>
+
+      <TouchableOpacity className="bg-blue-500 text-white p-2 rounded-md mt-4">
+        <Text className="text-white text-2xl p-2">Clique aqui</Text>
+      </TouchableOpacity>
     </View>
+
   );
 }
