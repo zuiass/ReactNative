9bee2c19197b0865992ad6371d5f2d74926faa6a
Josué <josuealexandre067@gmail.com>
2025-04-09 15:01:01 -0400

    update

--- app/cadastro.tsx
+++ app/cadastro.tsx
@@ -4,6 +4,22 @@ import { useEffect, useState } from "react";
 
 export default function Cadastro() {
 
+    const [usuario, setUsuario] = useState("");
+    const [nome, setNome] = useState("");
+    const [senha, setSenha] = useState("");
+
+    function entrarLogin() {
+        router.push("./");
+    }
+
+    function entrarHome() {
+        if (!usuario || !nome || !senha) {
+            return;
+        }
+
+        router.push("./home");
+    }
+
     return (
         <View className="flex-1 gap-4">
             <View className="flex w-full h-52 justify-center items-center gap-1">
@@ -12,8 +28,29 @@ export default function Cadastro() {
             </View>
 
             <View className="flex p-5 gap-3" style={{paddingTop: 0}}>
-                <TextInput className="p-3 rounded-full shadow-md" placeholder="Usuário" placeholderTextColor={"#9CA3AF"} underlineColorAndroid="transparent"/>
-                <TextInput className="p-3 rounded-full shadow-md" placeholder="Senha" placeholderTextColor={"#9CA3AF"} underlineColorAndroid="transparent"/>
+                <TextInput
+                    value={usuario}
+                    onChangeText={setUsuario}
+                    className="p-3 rounded-full shadow-md"
+                    placeholder="Usuário"
+                    placeholderTextColor={"#9CA3AF"}
+                />
+
+                <TextInput
+                    value={nome}
+                    onChangeText={setNome}
+                    className="p-3 rounded-full shadow-md"
+                    placeholder="Nome"
+                    placeholderTextColor={"#9CA3AF"}
+                />
+
+                <TextInput
+                    value={senha}
+                    onChangeText={setSenha}
+                    className="p-3 rounded-full shadow-md"
+                    placeholder="Senha"
+                    placeholderTextColor={"#9CA3AF"}
+                />
                 
                 <View className="w-full flex items-end">
                     <TouchableOpacity>
@@ -23,15 +60,15 @@ export default function Cadastro() {
             </View>
 
             <View className="flex p-5 gap-4">
-                <TouchableOpacity className="bg-indigo-600 rounded-full p-3 flex items-center">
+                <TouchableOpacity onPress={entrarHome} className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-full p-3 flex items-center">
                     <Text className="text-white">Cadastrar</Text>
                 </TouchableOpacity>
 
                 <View className="flex flex-row gap-2 justify-center">
-                    <Text className="text-gray-400">Não tem uma conta?</Text>
+                    <Text className="text-gray-400">Já tem uma conta?</Text>
 
-                    <TouchableOpacity>
-                        <Text className="font-bold">Criar</Text>
+                    <TouchableOpacity onPress={entrarLogin}>
+                        <Text className="font-bold">Entrar</Text>
                     </TouchableOpacity>
                 </View>
             </View>
--- app/index.tsx
+++ app/index.tsx
@@ -1,48 +1,67 @@
-import { Link, router } from "expo-router";
-import {Text, View, TouchableOpacity, Image, ScrollView, ActivityIndicator, TextInput} from "react-native";
-import { useEffect, useState } from "react";
+import { router } from "expo-router";
+import { Text, View, TouchableOpacity, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function Index() {
+
+    const [usuario, setUsuario] = useState("");
+    const [senha, setSenha] = useState("");
 
     function entrarCadastro() {
         router.push("./cadastro");
     }
 
     function entrarHome() {
-        router.push("./cadastro");
+        if (!usuario || !senha) {
+            return;
+        }
+
+        router.push("./home");
     }
 
     return (
-        <View className="flex-1 gap-4">
-            <View className="flex w-full h-52 justify-center items-center gap-1">
-                <Text className="text-3xl font-bold">Bem vindo</Text>
-                <Text className="text-gray-400">entre com a sua conta</Text>
-            </View>
+        <View className="flex-1 gap-4 bg-white">
+        <View className="flex w-full h-52 justify-center items-center gap-1">
+            <Text className="text-3xl font-bold">Bem vindo</Text>
+            <Text className="text-gray-400">entre com a sua conta</Text>
+        </View>
 
-            <View className="flex p-5 gap-3" style={{paddingTop: 0}}>
-                <TextInput className="p-3 rounded-full shadow-md focus:border-none" placeholder="Usuário" placeholderTextColor={"#9CA3AF"} />
-                <TextInput className="p-3 rounded-full shadow-md" placeholder="Senha" placeholderTextColor={"#9CA3AF"} />
-                
-                <View className="w-full flex items-end">
-                    <TouchableOpacity>
-                        <Text className="text-gray-400">Esqueceu a senha?</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+        <View className="flex p-5 gap-3" style={{ paddingTop: 0 }}>
+            <TextInput
+                value={usuario}
+                onChangeText={setUsuario}
+                className="p-3 rounded-full shadow-md"
+                placeholder="Usuário"
+                placeholderTextColor="#9CA3AF"
+            />
+            <TextInput
+                value={senha}
+                onChangeText={setSenha}
+                secureTextEntry
+                className="p-3 rounded-full shadow-md"
+                placeholder="Senha"
+                placeholderTextColor="#9CA3AF"
+            />
 
-            <View className="flex p-5 gap-4">
-                <TouchableOpacity onPress={entrarHome} className="bg-indigo-600 rounded-full p-3 flex items-center">
-                    <Text className="text-white">Entrar</Text>
+            <View className="w-full flex items-end">
+                <TouchableOpacity>
+                    <Text className="text-gray-400">Esqueceu a senha?</Text>
                 </TouchableOpacity>
-
-                <View className="flex flex-row gap-2 justify-center">
-                    <Text className="text-gray-400">Não tem uma conta?</Text>
-
-                    <TouchableOpacity onPress={entrarCadastro}>
-                        <Text className="font-bold">Criar</Text>
-                    </TouchableOpacity>
-                </View>
             </View>
+        </View>
+
+        <View className="flex p-5 gap-4">
+            <TouchableOpacity onPress={entrarHome} className="bg-indigo-600 rounded-full p-3 flex items-center">
+                <Text className="text-white">Entrar</Text>
+            </TouchableOpacity>
+
+            <View className="flex flex-row gap-2 justify-center">
+                <Text className="text-gray-400">Não tem uma conta?</Text>
+                <TouchableOpacity onPress={entrarCadastro}>
+                    <Text className="font-bold">Criar</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
         </View>
     );
 }
