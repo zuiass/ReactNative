b62489622df19aa8b7f8c377fd449a815e1368a9
Josué <josuealexandre067@gmail.com>
2025-04-11 17:14:54 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -12,8 +12,19 @@ const slides = [
 
 export default function App() {
 
+    const [feedback, setFeedback] = useState("");
+
+    // R E T U R N
+
     return (
         <View className="flex-1 p-5 bg-gray-900 gap-5">
+
+            <View
+            className="absolute top-5 left-5 z-50"
+            >
+            <Text className="text-white text-lg font-bold">kkkkk</Text>
+            </View>
+
 
             <View className="flex-1 bg-slate-800 rounded-2xl">
                 {/* Entrada de imagens */}
@@ -21,19 +32,18 @@ export default function App() {
 
             <View className="w-full h-[50px] box-border justify-center">
                 <TextInput
-                    className="flex-1 color-white bg-slate-800 rounded-full p-3 text-xl" style={{paddingRight: 50}}
-                    placeholder="Qual o local?"
+                    className="flex-1 color-white bg-slate-800 rounded-full p-3 text-xl" style={{paddingRight: 50, paddingLeft: 20}}
+                    placeholder="Esse lugar parece ser..."
                     placeholderTextColor={"#707070"}
                 />
 
-                <TouchableOpacity className="absolute right-0 bg-blue-700 rounded-full p-2" style={{marginRight: 7}}>
+                <TouchableOpacity className="absolute right-0 bg-violet-700 rounded-full p-2" style={{marginRight: 6}}>
                     <Image source={require('./send.svg')} resizeMode="cover"/>
                 </TouchableOpacity>
             </View>
         
         </View>
     );
-
 }
 
 // S T Y L E S
