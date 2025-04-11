ac750514dc4766b1f0d77df3de6063edb5fe7f1a
Josué <josuealexandre067@gmail.com>
2025-04-11 14:24:13 -0400

    Update index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { router, useLocalSearchParams } from "expo-router";
+import { Text, View, TouchableOpacity, TextInput } from "react-native";
+import { useState, useEffect } from "react";
+
+export default function Login() {
+
+    return (
+        <View className="flex-1 bg-white">
+
+        </View>
+    );
+
+}
