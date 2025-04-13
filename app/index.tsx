0101e41ea29acbc0f6473fabab0dd1e969394b8b
Josué <josuealexandre067@gmail.com>
2025-04-12 21:36:20 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -12,32 +12,33 @@ const slides = [
 
 export default function App() {
 
-    const [feedback, setFeedback] = useState("");
+    const [feedback, setFeedback] = useState("FFFFF");
 
     // R E T U R N
 
     return (
-        <View className="flex-1 p-5 bg-gray-900 gap-5 box-border p-5">
+        <View className="flex-1 max-h-[100%] bg-gray-900 box-border items-center">
 
-            <View className="absolute top-5 left-5 z-50 bg-red-700 w-[100%]">
-                <Text className="text-white text-lg font-bold">kkkkk</Text>
+            <View className="flex absolute z-10 mt-7 w-52 items-center bg-red-700 p-4 rounded-2xl">
+                <Text className="text-white text-lg font-bold">{feedback}</Text>
             </View>
 
+            <View className="flex-1 p-5 box-border gap-5">
+                <View className="flex-1 bg-slate-800 rounded-2xl">
+                    {/* Entrada de imagens */}
+                </View>
 
-            <View className="flex-1 bg-slate-800 rounded-2xl">
-                {/* Entrada de imagens */}
-            </View>
+                <View className="w-full h-[50px] box-border justify-center">
+                    <TextInput
+                        className="flex-1 color-white bg-slate-800 rounded-full p-3 text-xl" style={{paddingRight: 50, paddingLeft: 20}}
+                        placeholder="Esse lugar parece ser..."
+                        placeholderTextColor={"#707070"}
+                    />
 
-            <View className="w-full h-[50px] box-border justify-center">
-                <TextInput
-                    className="flex-1 color-white bg-slate-800 rounded-full p-3 text-xl" style={{paddingRight: 50, paddingLeft: 20}}
-                    placeholder="Esse lugar parece ser..."
-                    placeholderTextColor={"#707070"}
-                />
-
-                <TouchableOpacity className="absolute right-0 bg-violet-700 rounded-full p-2" style={{marginRight: 6}}>
-                    <Image source={require('./send.svg')} resizeMode="cover"/>
-                </TouchableOpacity>
+                    <TouchableOpacity className="absolute right-0 bg-violet-700 rounded-full p-2" style={{marginRight: 6}}>
+                        <Image source={require('./send.svg')} resizeMode="cover"/>
+                    </TouchableOpacity>
+                </View>
             </View>
         
         </View>
